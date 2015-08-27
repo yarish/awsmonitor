b75b5b6d2c06436253f3388cc1e0fb3085adc070
yarish <ykj@gmail.com>
2015-08-27 18:52:23 +0530

    adding logback logging along with SLF4j

--- awsmonitor/src/main/java/com/yarish/awsmonitor/App.java
+++ awsmonitor/src/main/java/com/yarish/awsmonitor/App.java
@@ -2,17 +2,19 @@ package com.yarish.awsmonitor;
 
 import java.util.HashMap;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.yarish.awsmonitor.model.Region;
 
 public class App {
-	private static final Logger LOG = Logger.getLogger(App.class.getName());
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
-		LOG.info("info");
+
 		HashMap<String, String> regions = Region.getRegions();
-		LOG.info("regions=" + regions);
+		logger.debug("regions=\n" + regions);
+		logger.info("regions=\n" + regions);
 	}
 
 }
